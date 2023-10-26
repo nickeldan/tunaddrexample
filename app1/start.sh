a162f6f08ad63c8734e8f2f0bfee6feef5a4483a
Daniel Walker <nickeldan@gmail.com>
2023-10-26 14:19:46 -0400

    removed the use of SIGUSR1

--- app1/start.sh
+++ app1/start.sh
@@ -1,20 +1,9 @@
 #!/bin/sh -ex
 
-./emit &
-pid=$!
-
-sleep 1
+./emit
 
 iptables -I FORWARD -i tun0 -o eth0 -j ACCEPT
 iptables -I FORWARD -i eth0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
-
-trigger_emit() {
-    while sleep 1; do
-        kill -s USR1 $pid
-    done
-}
-
-trigger_emit &
 
 tcpdump -i any -n
